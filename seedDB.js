b3b6dccd70c9945ef0f39e02ea4124cd25bfd231
danielvictory <danieljvictory@gmail.com>
2023-04-03 10:53:50 -0500

    format seedDB.js for legibility

--- seedDB.js
+++ seedDB.js
@@ -1,22 +1,18 @@
 // Require Node packages
 const express = require("express");
 const mongoose = require("mongoose");
-//const mO = require("method-override");
 require("dotenv").config();
 
 // Set up app
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const DATABASE_URL = process.env.DATABASE_URL
-
 // Set up varable with schema from model imports
 const Product = require("./models/products.js")
 const seed = require("./models/seed.js");
-//const { openDelimiter } = require("ejs");
 
 // Set up MongoDB connection through mongoose
-
+const DATABASE_URL = process.env.DATABASE_URL
 mongoose.connect(DATABASE_URL);
 const db = mongoose.connection;
 
