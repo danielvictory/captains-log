7dc57c4c4450446e58a008ff55c892d31995408a
danielvictory <danieljvictory@gmail.com>
2023-04-02 21:46:29 -0500

    seeded data in server.js file at first run and connect

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const DATABASE_URL = process.env.DATABASE_URL
 
 // Set up varable with schema from model imports
 const Product = require("./models/products.js")
+const seed = require("./models/seed.js")
 
 // Set up MongoDB connection through mongoose
 
@@ -23,6 +24,13 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Seed
+const seedDB = async () => {
+    await Product.deleteMany({});
+    await Product.insertMany(seed);
+}
+seedDB();
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
